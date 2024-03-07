58b310179dea874e678f5fb43e15402a4f0d644a
haben-tekeste <haben.t00@gmail.com>
2024-03-07 15:18:06 +0400

    Update api url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const BASE_URL = "https://polyadic2-6ea021d6a043.herokuapp.com/";
-export const BASE_URL = "http://localhost:4000/";
+export const BASE_URL = "https://polyadic2-6ea021d6a043.herokuapp.com/";
+// export const BASE_URL = "http://localhost:4000/";
 
 export const api = axios.create({
   baseURL: BASE_URL,
